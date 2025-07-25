b2940d888ac25df9af6c377124098b6638930881
saksham704 <sb031428@gmail.com>
2025-07-25 15:11:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let cartCount = 0;
-let cartItems = [];
-
-function addToCart(productName, price) {
-  cartItems.push({ productName, price });
-  cartCount++;
-  document.getElementById("cart-count").innerText = cartCount;
-  alert(productName + " added to cart!");
-}
